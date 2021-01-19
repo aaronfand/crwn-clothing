52e9534c1558139d76084bb0346f3862090998df
Aaron Fand <aaronf@thegraphicsstation.com>
2021-01-19 10:05:10 -0600

    CartUtils

--- src/pages/collection-preview/collection-preview.component.jsx
+++ src/pages/collection-preview/collection-preview.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './collection-preview.styles.scss';
-import ComponentItem from '../../components/collection-item/collection-item.component';
+import CollectionItem from '../../components/collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
 	<div className='collection-preview'>
@@ -10,8 +10,8 @@ const CollectionPreview = ({ title, items }) => (
 			{
 				items
 					.filter((item, idx) => idx < 4)
-					.map(({id,...OtherItemProps}) => (
-						<ComponentItem key={id} {...OtherItemProps} />
+					.map((item) => (
+						<CollectionItem key={item.id} item={item} />
 					))
 			}
 		</div>
