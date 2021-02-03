6d17ef86ef0835ba9f01844bcdd6e19db2317b88
Aaron Fand <aaronf@thegraphicsstation.com>
2021-02-03 16:32:12 -0600

    Container

---
+++ src/components/collections-overview/collections-overview.container.jsx
@@ -0,0 +1,19 @@
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import { selectIsComponentFetching } from '../../redux/shop/shop.selectors';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+import CollectionsOverview from './collections-overview.component';
+
+
+const mapStateToProps = createStructuredSelector({
+	isLoading: selectIsComponentFetching
+})
+
+const CollectionsOverviewContainer = compose(
+	connect(mapStateToProps),
+	WithSpinner
+)(CollectionsOverview);
+
+export default CollectionsOverviewContainer;
